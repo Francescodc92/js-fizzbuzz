acb306d3739b6438f16837c3b641aaac5b017105
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-06-14 16:39:35 +0200

    end bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,59 @@
+/*
+
+Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
+Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Come faccio a sapere se un numero è divisibile per un altro?
+Abbiamo visto qualcosa di particolare che possiamo usare?
+
+Ragionamento Base:
+1. scrivere un programma che stampi in console i numeri da 1 a 100
+  - ciclo da 1 a 100
+
+2. ma che per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
+  - condizione che verifica se il numero corrente sia divisibile per 3 e per 5
+    - cambio il testo in console dal numero al testo fizzbuzz 
+  - consizione che verifica se il numero è dividibile solo per 5
+    - cambio il testo in console dal numero al testo buzz 
+  - condizione che verifica se il numero è divisibile solo per 3 
+    - cambio il testo in console dal numero al testo fizz
+
+  se le condizioni precedenti non si verificano stampo il numero corrente 
+*/
+
+const rowElement = document.querySelector(".row")
+
+for (let i = 1; i <= 100; i++) {
+  let currentLog
+  let bgColor = "#1389b2"
+  if (i % 5 == 0 && i % 3 == 0) {
+    currentLog = "FizzBuzz"
+    bgColor = "#f0466f"
+  } else if (i % 5 == 0) {
+    currentLog = "Buzz"
+    bgColor = "#ffd166"
+  } else if (i % 3 == 0) {
+    currentLog = "Fizz"
+    bgColor = "#0cd6a1"
+  } else {
+    currentLog = i
+  }
+
+  renderHtml(currentLog, bgColor)
+}
+
+function renderHtml(currentLog, bgColor) {
+  const numberContainer = document.createElement("div")
+  numberContainer.classList.add(
+    "col-3",
+    "col-md-2",
+    "col-lg-1",
+    "number-container",
+    "border",
+    "text-center"
+  )
+  numberContainer.style.backgroundColor = bgColor
+  numberContainer.innerHTML = currentLog
+  rowElement.append(numberContainer)
+}
